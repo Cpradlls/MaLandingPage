66e4bc2e07f4695bee8a18b11483b3e3bc6ffdd4
Pradeilles Chloé <74190557+Cpradlls@users.noreply.github.com>
2021-03-08 16:22:18 +0100

    Delete session_start.php

--- conf/session_start.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-  // enforcing session cookie
-  @ini_set("session.cookie_httponly", 1); // XSS resistance
-  @ini_set("session.cookie_samesite", "Strict"); // CSRF resistance
-  session_name("Landing-page-session"); // say hello at humanize.me if you like my code
-  session_start(); // now we initiate the session
-
- 
