5bcfc5c540c2419c7a1ed1c3eff4078b71a95aa9
Pradeilles Chloé <74190557+Cpradlls@users.noreply.github.com>
2021-03-08 16:22:13 +0100

    Delete session_close.php

--- conf/session_close.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-  // cleaning up everything
-  if (!empty($_SESSION)) $_SESSION = [];
-  if (isset($_COOKIE[session_name()])) setcookie(session_name(), "", time()-1, "/"); // cookie viré
-  session_destroy();
